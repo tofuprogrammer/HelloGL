7164058eae7b7176bd8adc025df32894dc1305b2
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-13 09:44:03 +0000

    Started adding other shapes and cleaned up code

--- HelloGL.cpp
+++ HelloGL.cpp
@@ -3,8 +3,8 @@
 HelloGL::HelloGL(int argc, char* argv[]) {
 	GLUTCallbacks::Init(this); // Initializes the callback function
 	glutInit(&argc, argv); // Initializes the GLUT library
+	glutInitWindowSize(1920, 1080); // Sets the window size
 	glutCreateWindow("HelloGL");
-	glutInitWindowSize(800, 600); // Sets the window size
 	glutDisplayFunc(GLUTCallbacks::Display);
 	glutMainLoop();
 }
@@ -24,13 +24,23 @@ void HelloGL::Display()
 }
 
 void HelloGL::DrawPolygon() {
-	glBegin(GL_POLYGON); {
+	glBegin(GL_QUADS);
+		// Rectangle
 		glColor3f(1.0f, 0.0f, 1.0f); // Pink
-		glVertex2f(-0.75, 0.5);
-		glVertex2f(0.75, 0.5);
-		glColor3f(1.0f, 1.0f, 0.0f); // Brown
-		glVertex2f(0.75, -0.5);
-		glVertex2f(-0.75, -0.5);
+		glVertex2f(-0.85, 0.5);
+		glVertex2f(-0.95, 0.5);
+		glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+		glVertex2f(-0.95, -0.5);
+		glVertex2f(-0.85, -0.5);
 		glEnd();
-	}
+
+	glBegin(GL_TRIANGLES); 
+		// Equilateral triangle
+		glColor3f(1.0f, 1.0f, 1.0f); // White
+		glVertex2f(-0.75, -0.4);
+		glColor3f(1.0f, 0.0f, 0.5f); // Red-pink
+		glVertex2f(-0.8, -0.5);
+		glVertex2f(-0.7, -0.5);
+		glEnd();
+
 }
