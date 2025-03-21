047bc1bbb7515da0a0abedada4f8484a34aec78b
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-21 15:59:22 +0000

    Finished implementing cube

--- HelloGL.hpp
+++ HelloGL.hpp
@@ -10,45 +10,45 @@
 
 class Vector3 {
 public:
-	float x, y, z;
+    float x, y, z;
 };
 
 class Camera {
 public:
-	Vector3 eye;
-	Vector3 center;
-	Vector3 up;
-};
-
-class HelloGL
-{
-	Camera* camera;
-	float rotation;
-	static Vertex vertices[];
-	static Colour colours[];
-
-public:
-	//Constructor definition
-	HelloGL(int argc, char* argv[]);
-
-	// Destructor definition
-	~HelloGL(void);
-
-	void Display();
-	void DrawPolygon();
-	void DrawTorus();
-	void DrawCube();
-	void DrawCubeArray();
-	void Update();
-	void Keyboard(unsigned char key, int x, int y);
+    Vector3 eye;
+    Vector3 center;
+    Vector3 up;
 };
 
 class Colour {
 public:
-	GLfloat r, g, b;
+    GLfloat r, g, b;
 };
 
 class Vertex {
 public:
-	GLfloat x, y, z;
+    GLfloat x, y, z;
+};
+
+class HelloGL
+{
+    Camera* camera;
+    float rotation;
+    static Vertex vertices[];
+    static Colour colours[];
+
+public:
+    //Constructor definition
+    HelloGL(int argc, char* argv[]);
+
+    // Destructor definition
+    ~HelloGL(void);
+
+    void Display();
+    void DrawPolygon();
+    void DrawTorus();
+    void DrawCube();
+    void DrawCubeArray();
+    void Update();
+    void Keyboard(unsigned char key, int x, int y);
 };
