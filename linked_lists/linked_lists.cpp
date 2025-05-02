8f97f0998bfc28c5d3caa96ad9521a64aa3211a1
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-05-02 15:32:53 +0100

    Add `get_node` method to retrieve a node by position
    This method enables traversal of the linked list to find and return a node at a specific position with error handling for invalid positions.

--- linked_lists/linked_lists.cpp
+++ linked_lists/linked_lists.cpp
@@ -27,13 +27,13 @@ list_node* linked_lists::add_node(list_node** head, int data)
     }
 
     last_node->next_node = new_node;
-
     return new_node;
 }
 
 void linked_lists::delete_after(list_node* node)
 {
     list_node* temporary_pointer;
+
     if (node != nullptr && node->next_node != nullptr)
     {
         temporary_pointer = node->next_node;
@@ -53,13 +53,36 @@ void linked_lists::delete_list(list_node** node)
         delete temporary_pointer;
         temporary_pointer = next_node;
     }
+
     *node = nullptr;
 }
+
+list_node* linked_lists::get_node(list_node* node, int position)
+{
+    int counter = 0;
+
+    while (node != nullptr)
+    {
+        if (counter == position)
+        {
+            std::cout << "Data found at position " << position << " is " << node->data << '\n';
+            return node;
+        }
+        counter++;
+        node = node->next_node;
+    }
+
+    std::cout << "Position " << position << "is invalid\n";
+    return nullptr;
+}
+
 
 void linked_lists::insert_after(list_node* last_node, int data)
 {
     list_node* new_node = new list_node();
+
     new_node->data = data;
+
     new_node->next_node = last_node->next_node;
     last_node->next_node = new_node;
 }
@@ -67,10 +90,11 @@ void linked_lists::insert_after(list_node* last_node, int data)
 list_node* linked_lists::insert_first(list_node** head, int data)
 {
     list_node* new_node = new list_node();
+
     new_node->data = data;
+
     new_node->next_node = *head;
     *head = new_node;
-
     return new_node;
 }
 
