97f4fd91e3a01ca1942867815b72cc07a6f5555a
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-05-08 10:37:01 +0100

    Refactor the print_list function to use recursion for node traversal.
    I’ve updated the print_list function to replace the iterative while loop with a recursive method.

--- linked_lists/linked_lists.cpp
+++ linked_lists/linked_lists.cpp
@@ -198,15 +198,21 @@ list_node* linked_lists::insert_first(list_node** head, int data)
  */
 void linked_lists::print_list(list_node* node)
 {
+    /*
     while (node != nullptr)
     {
         std::cout << "| " << node->data << " |-->";
         node = node->next_node;
     }
-
+    */
     if (node == nullptr)
     {
         std::cout << "| NULL |\n";
+    }
+    else
+    {
+        std::cout << "| " << node->data << " |-->\n";
+        print_list(node->next_node);
     }
 }
 
