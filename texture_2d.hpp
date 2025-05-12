9efeaec2efbb4c32e9cfc5170150cc8bfbfac146
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-05-12 21:57:53 +0100

    Add texture_2d class for 2D texture management
    I started working on the `texture_2d` class with functionality to load and manage 2D textures using OpenGL. Updated the project configuration to include the newly added files: `texture_2d.cpp` and `texture_2d.hpp`.

---
+++ texture_2d.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "gl/freeglut.h"
+
+class texture_2d
+{
+private:
+    GLuint m_texture_id;
+    int m_width, m_height;
+
+public:
+    texture_2d();
+    ~texture_2d();
+
+    bool load_texture(char* path, int width, int height);
+
+    GLuint get_texture_id() const
+    {
+        return m_texture_id;
+    }
+    int get_width() const
+    {
+        return m_width;
+    }
+    int get_height() const
+    {
+        return m_height;
+    }
+};
